7b8b459f5f0af7a7b8b4d3930ac23b99fe72d9ad
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-31 13:02:38 +0530

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,8 +11,11 @@ function Login() {
             window.localStorage.setItem("password", "123");
             history.push("/profile");
         } else {
-            setUserName("");
-            setPassword("");
+            // setUserName("");
+            // setPassword("");
+            window.localStorage.setItem("username", username);
+            window.localStorage.setItem("password", password);
+            history.push("/profile");
         }
     }
 
